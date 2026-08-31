c3a75448b32ceb117772791ebe6d1374710fffb4
Dhriti Tandon <dhrititandon05@gmail.com>
2026-08-31 14:42:23 +0530

    Time: 7 ms (82.68%) | Memory: 48 MB (43.95%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low=1;
+        int lo=1;
         int high=0;
 
         for(int pile:piles){
             high=Math.max(high,pile);
         }
         int ans=high;
-        while(low<=high){
-            int mid=low+(high-low)/2;
+        while(lo<=high){
+            int mid=lo+(high-lo)/2;
             long hrs=0;
             for (int pile : piles) {
                 hrs += (pile + mid - 1) / mid;
@@ -17,7 +17,7 @@ class Solution {
                 ans = mid;
                 high = mid - 1;
             } else {
-                low = mid + 1;
+                lo = mid + 1;
             }
         }
         return ans;
